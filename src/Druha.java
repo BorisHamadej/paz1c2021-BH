cb75e7f0d5f7c460c82fc89883955f7db3dcd723
Alex Gajdoš <Alex Gajdo@158.197.237.25>
2021-09-21 13:39:27 +0200

    vytvorenie triedy Druha druhypokus

---
+++ src/Druha.java
@@ -0,0 +1,5 @@
+public class Druha {
+	public static void main(String[] args) {
+		System.out.println("vypis od kamosa");
+	}
+}
--- src/Prva.java
+++ src/Prva.java
@@ -1,7 +1,4 @@
 
 public class Prva {
-	
-	public static void main(String[] args) {
-		System.out.println("Prvy vypis");
-	}
+
 }
